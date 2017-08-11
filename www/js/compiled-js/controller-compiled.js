eb96cce55a84c4f060bcc80e0aab2f9ec73dd64d
Osahon Okungbowa <writeosahon@yahoo.co.uk>
2017-08-11 05:19:56 +0100

    initial commit - transaction history design and code implementation

--- www/js/compiled-js/controller-compiled.js
+++ www/js/compiled-js/controller-compiled.js
@@ -143,7 +143,14 @@ $('#app-main-navigator').get(0).bringPageTop("saved-recipients-page.html",{});//
 ons.notification.alert({title:"Security Check",messageHTML:'<ons-icon icon="md-close-circle-o" size="30px" '+'style="color: red;"></ons-icon> <span>'+'Security check failed. Invalid credentials'+'</span>',cancelable:true});}}).catch(function(){});return;}if(label=="intro"){// 'intro' button was clicked
 // close the side menu
 $('ons-splitter').get(0).left.close().then(function(){$('#app-main-navigator').get(0).bringPageTop("onboarding-page.html",{});// navigate to the onboarding page
-}).catch(function(){});return;}}},/**
+}).catch(function(){});return;}if(label=="transaction history"){// 'transaction history' button was clicked
+// close the side menu
+$('ons-splitter').get(0).left.close().then(function(){// ask user for secure PIN before proceeding. secure pin MUST match
+return ons.notification.prompt({title:"Security Check",id:"pin-security-check",class:"utopiasoftware-no-style",messageHTML:'<div><ons-icon icon="ion-lock-combination" size="24px" '+'style="color: #b388ff; float: left; width: 26px;"></ons-icon> <span style="float: right; width: calc(100% - 26px);">'+'Please enter your PostCash Secure PIN to proceed</span></div>',cancelable:true,placeholder:"Secure PIN",inputType:"number",defaultValue:"",autofocus:true,submitOnEnter:true});}).then(function(userInput){// user has provided a secured PIN , now authenticate it
+if(userInput===utopiasoftware.saveup.model.appUserDetails.securePin){// authentication successful
+$('#app-main-navigator').get(0).bringPageTop("transaction-history-page.html",{});// navigate to the specified page
+}else{// inform user that security check failed/user authentication failed
+ons.notification.alert({title:"Security Check",messageHTML:'<ons-icon icon="md-close-circle-o" size="30px" '+'style="color: red;"></ons-icon> <span>'+'Security check failed. Invalid credentials'+'</span>',cancelable:true});}}).catch(function(){});return;}}},/**
      * object is the view-model for the app security-pin-lock-modal
      */securityPinLockModalViewModel:{/**
          * property holds the input field jquery object used in the
@@ -411,7 +418,13 @@ $('#app-main-navigator').get(0).pushPage("saved-recipients-page.html",{});// nav
 }else{// inform user that security check failed/user authentication failed
 ons.notification.alert({title:"Security Check",messageHTML:'<ons-icon icon="md-close-circle-o" size="30px" '+'style="color: red;"></ons-icon> <span>'+'Security check failed. Invalid credentials'+'</span>',cancelable:true});}}).catch(function(){});return;}if(label=="intro"){// intro button was clicked
 $('#app-main-navigator').get(0).pushPage("onboarding-page.html",{});// navigate ot the onboarding page
-return;}}},/**
+return;}if(label=="transaction history"){// 'transaction history' button was clicked
+// ask user for secure PIN before proceeding. secure pin MUST match
+ons.notification.prompt({title:"Security Check",id:"pin-security-check",class:"utopiasoftware-no-style",messageHTML:'<div><ons-icon icon="ion-lock-combination" size="24px" '+'style="color: #b388ff; float: left; width: 26px;"></ons-icon> <span style="float: right; width: calc(100% - 26px);">'+'Please enter your PostCash Secure PIN to proceed</span></div>',cancelable:true,placeholder:"Secure PIN",inputType:"number",defaultValue:"",autofocus:true,submitOnEnter:true}).then(function(userInput){// user has provided a secured PIN , now authenticate it
+if(userInput===utopiasoftware.saveup.model.appUserDetails.securePin){// authentication successful
+$('#app-main-navigator').get(0).pushPage("transaction-history-page.html",{});// navigate to the transaction history pages
+}else{// inform user that security check failed/user authentication failed
+ons.notification.alert({title:"Security Check",messageHTML:'<ons-icon icon="md-close-circle-o" size="30px" '+'style="color: red;"></ons-icon> <span>'+'Security check failed. Invalid credentials'+'</span>',cancelable:true});}}).catch(function(){});return;}}},/**
      * object is view-model for verify-account page
      */verifyAccountPageViewModel:{/**
          * used to hold the parsley form validation object for the page
